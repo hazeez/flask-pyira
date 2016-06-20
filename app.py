ca25bb2ff949f7a34bae4009087a886e593076b5
hazeez <hafizul.azeez@gmail.com>
2016-06-20 14:38:38 +0530

    Incorporated login form

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash, redirect
+from forms import LoginForm
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 
 @app.route('/')
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('home.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Logged In User: %s' % form.username.data)
+        return redirect('/index')
+    return render_template('home.html',
+                           title='Sign In',
+                           form=form)
 
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-SERVER_URL="https://ofssjira.oraclecorp.com:8443/jira"
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'password-secret-key'
+SERVER_URL = "https://ofssjira.oraclecorp.com:8443/jira"
 USERNAME = 'hafizul.azeez@oracle.com'
 PASSWORD = '5rRN8Eabc1'
