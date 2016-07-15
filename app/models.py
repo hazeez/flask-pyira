b5a67583080119f6ad5292738bcfb5d25e8b3bc7
hazeez <hafizul.azeez@gmail.com>
2016-07-15 18:27:09 +0530

    included database scripts

--- app/models.py
+++ app/models.py
@@ -2,12 +2,12 @@ from app import db
 
 class Favorites(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	project_key = db.Column(db.String(120), index=True, unique=True)
-	username = db.Column(db.String(120), index=True, unique=True)
+	projectkey = db.Column(db.String(120), index=True, unique=True)
+	username = db.Column(db.String(120), index=True)
 
 	class Meta:
 		verbose_name = "FAVORITE"
 		verbose_name_plural = "FAVORITEs"
 
-	def __str__(self):
-		return "Project %s" % project_key
+	def __unicode__(self):
+		return "Project %s" % (self.projectkey)
